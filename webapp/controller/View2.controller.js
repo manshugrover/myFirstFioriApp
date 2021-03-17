8a8bef99601156514ca47e33c5581f03092cb361
vishalkwatra <vishal.kwatra88@gmail.com>
2021-03-17 11:53:43 +0530

    next comit

--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -15,7 +15,7 @@ sap.ui.define([
             },
             popupClose: function (choice) {
                 if (choice === "OK") {
-                    var oFirstTab=this.getView().byId("Zumba");
+                    var oFirstTab = this.getView().byId("Zumba");
                     oFirstTab.setVisible(false);
                     StatusMessage.show("your request has beeen approved");
                 }
@@ -27,24 +27,37 @@ sap.ui.define([
 
                 });
             },
-            cityPopup:null,
-            countryPopup:null,
-            onF4Help:function(){
-                StatusMessage.show("This page is under construction");
-                // this.cityPopup=new sap.ui.xmlfragment("oft.fiori.fragments.Popup", this);
-                // this.cityPopup.open();
+            cityPopup: null,
+            countryPopup: null,
+            onF4Help: function () {
+                // StatusMessage.show("This page is under construction");
+                this.cityPopup = new sap.ui.xmlfragment("oft.fiori.fragments.Popup", this);
+                this.getView().addDependent(this.cityPopup);
+
+                this.cityPopup.bindAggregation("items", {
+                    path: "/cities",
+                    template: new sap.m.StandardListItem({
+                        title: "{city}",
+                        description: "{famousFor}"
+                    })
+                });
+                this.cityPopup.open();
             },
-            onFilter:function(){
-                StatusMessage.show("This page is under construction");
-            //     this.countryPopup=new sap.ui.xmlfragment("oft.fiori.fragments.Popup", this);
-            //    this.countryPopup.bindAggrigation("items",{
-            //        path:"/countries",
-            //        template: new sap.m.StandardListItem({
-            //            title:"{name}",
-            //            description:"{code}"
-            //        })
-            //    })
-            //     this.countryPopup.open();
+            onFilter: function () {
+                // StatusMessage.show("This page is under construction");
+                this.countryPopup = new sap.ui.xmlfragment("oft.fiori.fragments.Popup", this);
+                //  this statement will give access to all what view knows-all the models
+                // which are available at view level
+                this.getView().addDependent(this.countryPopup);
+
+                this.countryPopup.bindAggregation("items", {
+                    path: "/countries",
+                    template: new sap.m.StandardListItem({
+                        title: "{name}",
+                        description: "{code}"
+                    })
+                });
+                this.countryPopup.open();
 
             }
         });
